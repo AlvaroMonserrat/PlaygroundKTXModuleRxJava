e3c96a08f0177866815c168106a13001733fc4a7
amonserrat <alvaro.monserrat@gmail.com>
2022-07-30 18:54:14 -0400

    Unit test

---
+++ app/src/test/java/com/rrat/playgroundktxmodulerxjava/fortesting/EngineTest.kt
@@ -0,0 +1,23 @@
+package com.rrat.playgroundktxmodulerxjava.fortesting
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class EngineTest{
+
+    private val engine = Engine(2000, 189, 15, false)
+    @Test
+    fun engineTurnsOn() {
+        engine.turnOn()
+        assertEquals(true, engine.isTurnedOn)
+        assertEquals(95, engine.temperature)
+    }
+
+    @Test
+    fun engineTurnsOff()
+    {
+        engine.turnOff()
+        assertEquals(false, engine.isTurnedOn)
+        assertEquals(15, engine.temperature)
+    }
+}
